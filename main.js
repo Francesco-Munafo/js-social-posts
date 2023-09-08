252f1bc6b4daf6004d83d994394abbebdeafe366
Francesco-Munafo <acquamarea1@gmail.com>
2023-09-08 16:32:10 +0200

    Add posts generation Add like button function

--- main.js
+++ main.js
@@ -112,11 +112,72 @@ Non è necessario creare date casuali
 Per le immagini va bene utilizzare qualsiasi servizio di placeholder ad es. Unsplash (https://unsplash.it/300/300?image=<id>)
 */
 
-const userPic = document.querySelector('post-meta__icon');
+
+/*
+
+Milestone 2
+Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed.
+
+*/
+
+const postContainer = document.getElementById('container');
 
 
-posts.forEach(post => {
+for (let i = 0; i < posts.length; i++) {
 
-    userPic.innerHTML = post.author.image;
+    const postMarkup = `
+    <div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="${posts[i].author.image}" alt="">                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${posts[i].author.name}</div>
+                        <div class="post-meta__time">${posts[i].created}</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">${posts[i].content}</div>
+            <div class="post__image">
+                <img src="${posts[i].media}" alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="${posts[i].id}">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-${posts[i].id}" class="js-likes-counter">${posts[i].likes}</b> persone
+                    </div>
+                </div> 
+            </div>            
+        </div>
+    `
+    postContainer.innerHTML += postMarkup
+
+}
+
+/*
+
+Milestone 3
+Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo. Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
+
+*/
+
+    const postHTML = document.querySelectorAll('.likes');
+    console.log(postHTML);
+
+postHTML.forEach(post => {
+    const likeButton = document.querySelector('.like-button');
+    console.log(likeButton);
+    likeButton.addEventListener('click', function submitLike(){
+        likeButton.classList.toggle('text-primary')
+        post.likes++
+    })
     
 });
+
