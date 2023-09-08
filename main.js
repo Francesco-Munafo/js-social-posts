c8ec7be14b857369135e52876707c995bbe6b96c
Francesco-Munafo <acquamarea1@gmail.com>
2023-09-08 17:16:28 +0200

    Add new like function

--- main.js
+++ main.js
@@ -120,46 +120,50 @@ Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i 
 
 */
 
-const postContainer = document.getElementById('container');
 
-
-for (let i = 0; i < posts.length; i++) {
-
-    const postMarkup = `
-    <div class="post">
-            <div class="post__header">
-                <div class="post-meta">                    
-                    <div class="post-meta__icon">
-                        <img class="profile-pic" src="${posts[i].author.image}" alt="">                    
+function postGen(array) {
+    const postContainer = document.getElementById('container');
+    
+    for (let i = 0; i < array.length; i++) {
+    
+        const postMarkup = `
+        <div class="post">
+                <div class="post__header">
+                    <div class="post-meta">                    
+                        <div class="post-meta__icon">
+                            <img class="profile-pic" src="${array[i].author.image}" alt="">                    
+                        </div>
+                        <div class="post-meta__data">
+                            <div class="post-meta__author">${array[i].author.name}</div>
+                            <div class="post-meta__time">${array[i].created}</div>
+                        </div>                    
                     </div>
-                    <div class="post-meta__data">
-                        <div class="post-meta__author">${posts[i].author.name}</div>
-                        <div class="post-meta__time">${posts[i].created}</div>
-                    </div>                    
                 </div>
+                <div class="post__text">${array[i].content}</div>
+                <div class="post__image">
+                    <img src="${array[i].media}" alt="">
+                </div>
+                <div class="post__footer">
+                    <div class="likes js-likes">
+                        <div class="likes__cta">
+                            <a class="like-button  js-like-button" href="#" data-postid="${array[i].id}">
+                                <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                                <span class="like-button__label">Mi Piace</span>
+                            </a>
+                        </div>
+                        <div class="likes__counter">
+                            Piace a <b id="like-counter-${array[i].id}" class="js-likes-counter">${array[i].likes}</b> persone
+                        </div>
+                    </div> 
+                </div>            
             </div>
-            <div class="post__text">${posts[i].content}</div>
-            <div class="post__image">
-                <img src="${posts[i].media}" alt="">
-            </div>
-            <div class="post__footer">
-                <div class="likes js-likes">
-                    <div class="likes__cta">
-                        <a class="like-button  js-like-button" href="#" data-postid="${posts[i].id}">
-                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                            <span class="like-button__label">Mi Piace</span>
-                        </a>
-                    </div>
-                    <div class="likes__counter">
-                        Piace a <b id="like-counter-${posts[i].id}" class="js-likes-counter">${posts[i].likes}</b> persone
-                    </div>
-                </div> 
-            </div>            
-        </div>
-    `
-    postContainer.innerHTML += postMarkup
-
+        `
+        postContainer.innerHTML += postMarkup
+    
+    }
 }
+
+postGen(posts)
 
 /*
 
@@ -167,17 +171,18 @@ Milestone 3
 Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo. Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
 
 */
+//Creo una funzione per l'aggiunta del like
+function addLike(likeCount) {
+const likeButton = document.querySelector('.like-button'); //
 
-    const postHTML = document.querySelectorAll('.likes');
-    console.log(postHTML);
-
-postHTML.forEach(post => {
-    const likeButton = document.querySelector('.like-button');
+likeButton.addEventListener('click', function () {
     console.log(likeButton);
-    likeButton.addEventListener('click', function submitLike(){
-        likeButton.classList.toggle('text-primary')
-        post.likes++
-    })
+    likeCount++
+    likeButton.classList.add('text-primary')
+})
     
-});
+    postGen(posts)
+}
+
+addLike(posts.likes)
 
